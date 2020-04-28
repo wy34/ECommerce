09a4417e17850f96bde7489f4ec06ec41509bf0a
William Yeung <williamyeung1141998@gmail.com>
2020-04-28 19:59:27 -0400

    Initial commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by William Yeung on 4/28/20.
+//  Copyright © 2020 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
